4dcfa2499d492e40252affa0e6d52690fa29ecbd
kumarbaberwal <niteshkumarghn1@gmail.com>
2025-04-01 02:02:27 +0530

    Added QuestionSchema

---
+++ backend/src/models/questions.ts
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const QuestionSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    language: {
+        type: String,
+        required: true,
+    },
+    tags: {
+        type: [{ type: String }],
+        required: true,
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    aiGeneratedSolution: {
+        type: String,
+    },
+    votes: {
+        type: Number,
+        default: 0,
+    },
+    views: {
+        type: Number,
+        default: 0,
+    },
+}, { timestamps: true });
+
+
+export const Question = mongoose.model('Question', QuestionSchema);
