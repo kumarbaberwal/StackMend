7d46712e3441c463fad9ffe5d665372f49699509
kumarbaberwal <niteshkumarghn1@gmail.com>
2025-04-12 16:53:39 +0530

    navigation - added

--- web/app/(auth)/forgetpassword/page.tsx
+++ web/app/(auth)/forgetpassword/page.tsx
@@ -1,7 +1,15 @@
+"use client"
+
+import { useRouter } from "next/navigation";
+
 export default function ForgetPassword() {
+    const router = useRouter();
     return (
         <div>
             This is the Forget Password Page
+            <br />
+            Go to Home Page:
+            <button onClick={() => router.push('/')} className="text-3xl text-amber-300 text-center">Home</button>
         </div>
     );
 }
---
+++ web/components/navigation.tsx
@@ -0,0 +1,16 @@
+"use client"
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function Navigation() {
+    const pathName = usePathname();
+    return (
+        <nav className="justify-center items-center flex">
+            <Link href={'/'} className={pathName === '/' ? "mr-4 text-2xl text-red-400" : "mr-4 text-2xl text-blue-300"}> Home </Link>
+            <Link href={'/signup'} className="mr-4 text-2xl text-blue-300"> Signup </Link>
+            <Link href={'/login'} className="mr-4 text-2xl text-blue-300"> Login </Link>
+            <Link href={'/forgetpassword'} className="mr-4 text-2xl text-blue-300"> Forget Password </Link>
+            <Link href={'/error/102'} className="mr-4 text-2xl text-blue-300"> Error </Link>
+        </nav>
+    );
+}
